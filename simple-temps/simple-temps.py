32e2f24706ac0c794b236950bfd29554e9e2cca7
nnshah2 <neelay.n.shah@gmail.com>
2021-04-17 08:52:15 -0700

    elevating python

--- simple-temps/simple-temps.py
+++ simple-temps/simple-temps.py
@@ -1,3 +1,15 @@
+import sys
+import ctypes
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+    
+if not is_admin():
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+
 import kivy
 import wmi
 import subprocess
@@ -8,21 +20,27 @@ from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
 import kivy.properties as properties
 from kivy.clock import Clock
-
+from kivy.lang import Builder
+import kivy.properties as properties
+from kivy.clock import Clock
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.button import Button
 
 datalist = []
 
 sensors = wmi.WMI(namespace='root\OpenHardwareMonitor')
 
+
 def HardwareMonitorOpen():
     script_directory = os.path.dirname(os.path.abspath(__file__))
     parent_directory = os.path.dirname(script_directory)
     hardware_monitor_path = os.path.join(parent_directory,"thirdparty","openhardwaremonitor-v0.9.6\OpenHardwareMonitor\OpenHardwareMonitor.exe")
-#    print(hardware_monitor_path,flush=True)
-    return subprocess.Popen([hardware_monitor_path],shell=True)
+    return subprocess.Popen([hardware_monitor_path])
+
 
 def HardWareMonitorClose(process_):
     process_.kill()
+    process_.terminate()
 
 class NumDef(Widget):
     numdef = properties.NumericProperty(0)
@@ -49,7 +67,13 @@ class TempRead(GridLayout):
                     self.cpupercent.numdef = int(datatuple[1])
                 elif 'GPU Core Load' in datatuple[0]:
                     self.gpupercent.numdef = int(datatuple[1])
-        
+
+class SecondWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+                    
 class TempApp(App):
     def build(self):
         temps = TempRead()
@@ -57,6 +81,8 @@ class TempApp(App):
         return temps
       
 if __name__ == '__main__':
+    if not is_admin():
+        exit()
     HardwareMonitor = HardwareMonitorOpen()
     atexit.register(HardWareMonitorClose, HardwareMonitor)
     TempApp().run()
